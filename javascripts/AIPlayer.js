0ddc0cbd835aa0f52072b36cd1eb7edcceede089
Gordey Chernyy <kewava@gmail.com>
2017-09-28 16:01:27 -0400

    add quotes, change canvas size

--- javascripts/AIPlayer.js
+++ javascripts/AIPlayer.js
@@ -1,16 +1,17 @@
 var AIPlayer = function() {
     this.smooth = 0.97;
-    this.sprite = createSprite(windowWidth - 10, windowHeight, 10, 100);
+    this.sprite = createSprite(width - 10, height, 10, 100);
     this.sprite.immovable = true;
-    this.pos = new p5.Vector(windowWidth - 20, windowHeight/2);
+    this.pos = new p5.Vector(width - 20, height/2);
 }
 AIPlayer.prototype.getPaddle = function() {
 	return this.sprite;
 };
 AIPlayer.prototype.update = function(target) {
 	// calculate follow the target
-	var isTargetClose = target.x > windowWidth / 2 ? true : false;
-	var randomTarget = noise(frameCount/100.0)*windowHeight;
+
+	var isTargetClose = target.x > width / 2 ? true : false;
+	var randomTarget = noise(frameCount/100.0)*height;
 	var targetPos = isTargetClose ? target.y : randomTarget;
     this.pos.y = this.pos.y * this.smooth + targetPos * (1 - this.smooth); // xeno smooth
     
--- sketch.js
+++ sketch.js
@@ -1,9 +1,15 @@
 // canvas
 var canvas;
+var width;
+var height;
 
 // player
 var playerPos;
 var player;
+
+// text
+var quote;
+var quoteStrings;
 
 // images
 var eyeImage;
@@ -24,6 +30,9 @@ var ball, ballImage;
 var wallTop, wallBottom;
 
 function preload() {
+    // text
+    quoteStrings = {};
+    quoteStrings['Dekart'] = loadStrings('Assets/text/quoteDekart1.txt');
     // sounds
     looseSound = loadSound('Assets/sounds/pLoose.mp3');
     winSound = loadSound('Assets/sounds/pWin.mp3');
@@ -40,17 +49,25 @@ function preload() {
 //     SETUP
 // -------------
 function setup() {
-    canvas = createCanvas(windowWidth, windowHeight);
+    canvas = createCanvas(1000, 640);
     canvas.parent('myContainer');
-    canvas.position(0, 0);
+    width = canvas.width;
+    height = canvas.height;
 
+    setupQuote();
     setupSoundPlayer();
     setupWalls();
     setupAIPlayer();
     setupPlayer();
     setupBall();
 }
-function setupSoundPlayer(){
+
+function setupQuote(argument) {
+    quote = new Quote();
+    quote.setQuotes(quoteStrings);
+}
+
+function setupSoundPlayer() {
     soundPlayer = new SoundPlayer();
     // collect sounds in soundPlayer
     soundPlayer.add('looseSound', looseSound);
@@ -58,14 +75,16 @@ function setupSoundPlayer(){
     soundPlayer.add('pongHitSound', pongHitSound);
     soundPlayer.add('pongWallSound', pongWallSound);
 }
+
 function setupAIPlayer() {
     aiPlayer = new AIPlayer();
 }
 
+
 function setupWalls() { // define walls here becuase we can have two balls but walls will be the same
-    wallTop = createSprite(windowWidth / 2, -30 / 2, windowWidth, 30);
+    wallTop = createSprite(width / 2, -30 / 2, width, 30);
     wallTop.immovable = true; // should collide with ball
-    wallBottom = createSprite(windowWidth / 2, windowHeight + 30 / 2, windowWidth, 30);
+    wallBottom = createSprite(width / 2, height + 30 / 2, width, 30);
     wallBottom.immovable = true; // should collide with ball
 }
 
@@ -81,7 +100,8 @@ function setupPlayer() {
     player.setupEyes(eyeImage, new p5.Vector(71, 54), new p5.Vector(99, 54)); // eye pos in px mesured in photoshop according to sprite
     // subscribe to event when ball loose
 }
-function setupBall(){
+
+function setupBall() {
     // setup ball
     ball = new Ball(ballImage);
     // pass all collider to ball
@@ -90,7 +110,7 @@ function setupBall(){
     ball.paddleRight = aiPlayer.getPaddle(); // using getPaddle we can return any collided part of the player body (ex. hand or head )
     ball.paddleLeft = player.getPaddle();
     // subscribe to ball events, pass the function as variable that should run on any ball event
-    ball.onLoose(player.onLoose); 
+    ball.onLoose(player.onLoose);
     ball.onWin(player.onWin);
     // subscribe sounds to ball events
     ball.onLoose(soundPlayer.play['looseSound']);
@@ -98,18 +118,26 @@ function setupBall(){
     ball.onLeftCollide(soundPlayer.play['pongHitSound']);
     ball.onRightCollide(soundPlayer.play['pongHitSound']);
     ball.onWallCollide(soundPlayer.play['pongWallSound']);
+    // quote 
+    ball.onLeftCollide(quote.revealWord);
+    ball.onLoose(quote.hideAllQuote);
 }
 // -------------
 //     UPDATE
 // -------------
 function draw() {
     background(0, 0, 0);
-    
+
     updatePlayer();
     updateAIPlayer();
     updateBall();
 
+    drawQuote();
     drawSprites();
+}
+
+function drawQuote() {
+    quote.draw();
 }
 
 function updateBall() {
